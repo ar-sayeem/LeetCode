da0acd7c8f194a19d95c06982e99ddd9a211bd6e
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2025-01-06 23:36:54 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,22 +1,21 @@
 class Solution(object):
-    def romanToInt(self, s):
-        roman = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000,
-        }
-
-        total = 0
-        for i in range(len(s) - 1):
-            if roman[s[i]] < roman[s[i+1]]:
-                total -= roman[s[i]]
-            else:
-                total += roman[s[i]]
-        return total + roman[s[-1]]
-#                       last digit
-
-        
+ def romanToInt(self, s):
+    roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    total = 0
+    s = s.replace('IV', 'IIII')
+    s = s.replace('IX', 'VIIII')
+    S = s.replace('XL', 'XXXX')
+    s = s.replace('XC', 'LXXXX')
+    s = s.replace('CD', 'CCCC')
+    s = s.replace('CM', 'DCCCC')
+    for numeral in s:
+        total += roman[numeral]
+    return total      

95d51229a03ea7f5e4c256dc3e22c84999a47711
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-01-17 22:12:17 +0600

    Time: 3 ms (81.1%), Space: 19.3 MB (17.06%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,21 +1,28 @@
-class Solution(object):
- def romanToInt(self, s):
-    roman = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    total = 0
-    s = s.replace('IV', 'IIII')
-    s = s.replace('IX', 'VIIII')
-    S = s.replace('XL', 'XXXX')
-    s = s.replace('XC', 'LXXXX')
-    s = s.replace('CD', 'CCCC')
-    s = s.replace('CM', 'DCCCC')
-    for numeral in s:
-        total += roman[numeral]
-    return total      
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            if i < n - 1 and roman_values[s[i]] < roman_values[s[i + 1]]:
+                total -= roman_values[s[i]]
+            else:
+
+                total += roman_values[s[i]]
+        
+        return total
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# by ar-sayeem [January 17, 2026]

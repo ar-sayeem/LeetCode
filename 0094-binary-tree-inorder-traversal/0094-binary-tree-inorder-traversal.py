2364e5796573fedc730154452b62d1d00a15d49f
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-02-02 10:33:52 +0600

    Time: 0 ms (100%), Space: 19.4 MB (32.59%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = []
+        curr = root
+
+        while curr or stack:        # if both curr and stack is available
+            while curr:             # if curr available go left as long as possible
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()      # if pop, add it's value to result
+            res.append(curr.val)
+            curr = curr.right       # since value is added of left node, time to do same for right nodes
+        return res
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# by ar-sayeem [February 2, 2026]

a3e343b507b45244f3d07be8f492d7a566d183f1
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-02-03 11:36:04 +0600

    Time: 0 ms (100%), Space: 19.3 MB (33.82%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -6,18 +6,45 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ### Morris Inorder Traversal ###
         res = []
-        stack = []
         curr = root
 
-        while curr or stack:        # if both curr and stack is available
-            while curr:             # if curr available go left as long as possible
-                stack.append(curr)
-                curr = curr.left
-            curr = stack.pop()      # if pop, add it's value to result
-            res.append(curr.val)
-            curr = curr.right       # since value is added of left node, time to do same for right nodes
+        while curr:
+            if not curr.left:
+                res.append(curr.val)
+                curr = curr.right
+            else:
+                predecessor = curr.left         # predecessor = The rightmost node in curr’s left subtree.
+                while predecessor.right and predecessor.right != curr:
+                    predecessor = predecessor.right
+                
+                if not predecessor.right:
+                    predecessor.right = curr
+                    curr = curr.left
+                else:
+                    predecessor.right = None
+                    res.append(curr.val)
+                    curr = curr.right
         return res
+  
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# by ar-sayeem [February , 2026]
+
+        
+        # res = []
+        # stack = []
+        # curr = root
+
+        # while curr or stack:        # if both curr and stack is available
+        #     while curr:             # if curr available go left as long as possible
+        #         stack.append(curr)
+        #         curr = curr.left
+        #     curr = stack.pop()      # if there is nothing in left pop, then add it's value to result
+        #     res.append(curr.val)
+        #     curr = curr.right       # since value is added of left node, time to do same for right nodes
+        # return res
 
 # Time Complexity: O(n)
 # Space Complexity: O(n)

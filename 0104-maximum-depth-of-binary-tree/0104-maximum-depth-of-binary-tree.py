2cc628e0cfc1395086d5b04295a01292c1ae955a
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-02-07 13:06:23 +0600

    Time: 0 ms (100%), Space: 20.4 MB (33.77%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,12 +6,50 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+# Approch 3: Iterative DFS (Pre-order)
+        if not root:
+            return 0
         
-        # Approch 1: DFS – Recursive
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
+
+# Time Complexity: O(n) || Space Complexity: O(n)
+        
+"""      
+# Approch 1: DFS – Recursive
         if not root:    # NULL tree
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
         # If not NULL then at leat 1, then recursive for child cases
     
-# Time Complexity: O(n)
-# Space Complexity: O(n)
+# Time Complexity: O(n) || Space Complexity: O(n)
+"""
+"""
+# Approch 2: BFS (level-order traversal)
+        if not root:
+            return 0
+        
+        level = 0
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
+        
+# Time Complexity: O(n) || Space Complexity: O(n)
+"""
+

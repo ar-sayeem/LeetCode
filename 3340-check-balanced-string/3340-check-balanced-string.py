e8f6a50e527a353b737f8fffbf5b5daa0e34ddfe
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-02-09 15:19:42 +0600

    Time: 3 ms (41.81%), Space: 19.5 MB (10.88%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isBalanced(self, num: str) -> bool:
+        even_sum = 0
+        odd_sum = 0
+
+        for i, digit in enumerate(num):
+            if i % 2 == 0:
+                even_sum += int(digit)
+            else:
+                odd_sum += int(digit)
+
+        return even_sum == odd_sum      # returns a boolean
+
+
+# Time Complexity: O()
+# Space Complexity: O()
+# by ar-sayeem [Month Date, 2026]

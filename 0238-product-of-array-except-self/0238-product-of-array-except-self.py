d6efce5fa803002dcea49205ae139cf640cd09aa
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-03-12 15:59:18 +0600

    Time: 19 ms (77.12%), Space: 25.7 MB (52.1%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,18 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        l_mult = 1
-        r_mult = 1
         n = len(nums)
-        l_arr = [1] * n
-        r_arr = [1] * n
-        
-        for i in range(n):
-            j = -i - 1
-            
-            l_arr[i] = l_mult
-            r_arr[j] = r_mult
-            
-            l_mult *= nums[i]
-            r_mult *= nums[j]
-        
-        return [l * r for l, r in zip(l_arr, r_arr)]
+        ans = [1] * n          # output array, prefilled with 1s
+
+        left = 1
+        for i in range(n):     # left to right
+            ans[i] *= left     # store left product at current index
+            left *= nums[i]    # update left product to include current element
+
+        right = 1
+        for i in range(n - 1, -1, -1):  # right to left-> range(start, stop, step)
+            ans[i] *= right             # multiply existing left product by right product
+            right *= nums[i]            # update right product to include current element
+
+        return ans  # each ans[i] now holds left_product × right_product = product of all except self
+
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+# by ar-sayeem [March 12, 2026]

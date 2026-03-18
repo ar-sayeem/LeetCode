b602de36f2b7056ad5b9c29790a9db30788a01d4
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-03-18 20:07:02 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
 
@@ -8,28 +10,14 @@ class Solution:
         prefixSum = 0
 
         for i in nums:
-            prefixSum += i
-            if prefixSum - k in sumCountMap:
-                result += sumCountMap[prefixSum - k]
-            
-            sumCountMap[prefixSum] += 1
+            prefixSum += i                              # move one step forward
+            if prefixSum - k in sumCountMap:            # did we see this before?
+                result += sumCountMap[prefixSum - k]    # yes! count those subarrays
+            sumCountMap[prefixSum] += 1                 # record this stop in notebook
         
         return result
-    
+
 
 # Time Complexity: O(N)
 # Space Complexity: O(N)
 # by ar-sayeem [March 18, 2026]
-
-
-
-
-        # N = len(nums)
-        # count = 0
-        # for i in range(N):      # loop 0 to N
-        #     prefixSum = 0
-        #     for j in range(i, N):   # loop i to N
-        #         prefixSum += nums[j]
-        #         if prefixSum == k:
-        #             count += 1
-        # return count

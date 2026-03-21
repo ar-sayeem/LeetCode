fcac54d17c83590bce6b9964719ba159baa203f7
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-03-21 11:02:03 +0600

    Time: 0 ms (100%), Space: 19.5 MB (93.55%) - LeetHub

---
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.py
@@ -0,0 +1,37 @@
+from typing import List
+
+class Solution:
+    def reverseSubmatrix(self, grid: List[List[int]], x: int, y: int, k: int) -> List[List[int]]:
+        # top = first row of the submatrix
+        top, bot = x, x + k -1
+        # bot = last row of the submatrix
+
+        while top < bot:
+            # swap two rows within the submatrix
+            grid[top][y : y + k], grid[bot][y : y + k] = grid[bot][y : y + k], grid[top][y : y + k]
+            top += 1
+            bot -= 1
+
+        return grid
+
+
+# Time Complexity:  O(k²)
+# Space Complexity: O(k)
+# by ar-sayeem [March 20, 2026]
+
+
+#       🌙✨ Eid ul-Fitr Mubarak ✨🌙
+#
+#      ╔══════════════════════════════╗
+#      ║      🌙 Eid Mubarak 🌙      ║
+#      ║   May your heart be filled   ║
+#      ║   with joy, your home with   ║
+#      ║   laughter, and your life    ║
+#      ║   with endless blessings.    ║
+#      ║                              ║
+#      ║   Celebrate love, kindness,  ║
+#      ║    and togetherness today!   ║
+#      ╚══════════════════════════════╝
+#
+# 🕌 Wishing you peace, happiness, and prosperity!
+# 🌟 Eid ul-Fitr Mubarak to you and your family! 🌟

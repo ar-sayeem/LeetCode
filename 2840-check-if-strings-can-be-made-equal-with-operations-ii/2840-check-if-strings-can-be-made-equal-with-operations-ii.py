10ceb93433a15c2ad02f049807540eae1fb58e37
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-03-30 10:37:30 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -1,13 +1,15 @@
+from collections import Counter
+
 class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
-        # index 0,2,4... from both → sort → check if same
-        even_match = sorted(s1[0::2]) == sorted(s2[0::2])
-        # grab index 1,3,5... from both → sort → check if same
-        odd_match = sorted(s1[1::2]) == sorted(s2[1::2])
+        # count even index characters from both → check if same
+        even_match = Counter(s1[0::2]) == Counter(s2[0::2])
+        # count odd index characters from both → check if same
+        odd_match = Counter(s1[1::2]) == Counter(s2[1::2])
 
         return even_match and odd_match
 
 
-# Time: O(n log n)
-# Space: O(n)
+# Time: O(n)
+# Space: O(1)   → Counter has at most 26 keys
 # by ar-sayeem [March 30, 2026]

26547137b3ae30e2bb20291de9b1a26dd2d38166
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-05-10 23:06:28 +0600

    Time: 11 ms (68.13%), Space: 19.6 MB (91.29%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,39 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        col = set()
+        posDiag = set()     # (r + c)
+        negDiag = set()     # (r - c)
+
+        ans = []
+        board = [["."] * n for i in range(n)]
+
+        def backTract(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                ans.append(copy)
+                return
+
+            for i in range(n):
+                if i in col or (r + i) in posDiag or (r - i) in negDiag:
+                    continue
+
+                col.add(i)
+                posDiag.add(r + i)
+                negDiag.add(r - i)
+                board[r][i] = "Q"
+
+                backTract(r + 1)
+
+                col.remove(i)
+                posDiag.remove(r + i)
+                negDiag.remove(r - i)
+                board[r][i] = "."
+
+        backTract(0)
+
+        return ans
+
+
+# Time Complexity   : O(N!)
+# Space Complexity  : O(N)
+# by ar-sayeem [May 10, 2026]

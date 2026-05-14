e71c51484956ebbad0971432dd1280afbef41baa
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-05-14 09:45:01 +0600

    Time: 0 ms (100%), Space: 19.3 MB (17.79%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -1,14 +1,27 @@
 from typing import List
+from collections import Counter
 
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
-        nums.sort()  # sorted nums List
         n = len(nums)
+        freq = Counter(nums)
 
-        return nums == list(range(1, n)) + [n - 1]
-        #             [1, 2, ..., n - 1] + [n - 1]
+        # numbers 1 to n-2 should appear once
+        for i in range(1, n - 1):
+            if freq[i] != 1:
+                return False
+        
+        # n-1 should appear twice
+        return freq[n - 1] == 2     # if not return False
 
 
-# Time Complexity   : O(N log N)
+# Time Complexity   : O(N)
 # Space Complexity  : O(N)
 # by ar-sayeem [May 14, 2026]
+
+        # nums.sort()  # sorted nums List
+        # n = len(nums)
+
+        # return nums == list(range(1, n)) + [n - 1]
+        #             [1, 2, ..., n - 1] + [n - 1]
+        # O(N log N) | O(N)

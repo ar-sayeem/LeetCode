c3a9a2c8428539cc957886f1be3967af55a16c04
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-06-03 05:52:22 +0600

    Time: 11 ms (67.05%), Space: 19.4 MB (17.92%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
@@ -9,6 +9,7 @@ class Solution:
         waterStartTime: List[int],
         waterDuration: List[int],
     ) -> int:
+
         def solve(start1, duration1, start2, duration2):
             finish1 = inf
             for i in range(len(start1)):
@@ -18,10 +19,11 @@ class Solution:
             for i in range(len(start2)):
                 finish2 = min(finish2, max(start2[i], finish1) + duration2[i])
             return finish2
+        
+        landWater = solve(landStartTime, landDuration, waterStartTime, waterDuration)
+        waterLand = solve(waterStartTime, waterDuration, landStartTime, landDuration)
 
-        land_water = solve(landStartTime, landDuration, waterStartTime, waterDuration)
-        water_land = solve(waterStartTime, waterDuration, landStartTime, landDuration)
-        return min(land_water, water_land)
+        return min(landWater, waterLand)
 
 
 # Time Complexity: O(N + M)

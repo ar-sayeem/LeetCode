70a7bd7cdaed7ff9c878b07e78040b087125c550
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-06-07 13:48:20 +0600

    Time: 0 ms (100%), Space: 10.3 MB (8.88%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,31 @@
+#include <stack>
+using namespace std;
+
+class MyQueue {
+public:
+    stack<int> s1;
+    stack<int> s2;
+    MyQueue() {}
+    void push(int x) {
+        while (!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s1.push(x);
+        while (!s2.empty()) {
+            s1.push(s2.top());
+            s2.pop();
+        }
+    }
+    int pop() {
+        int ans = s1.top();
+        s1.pop();
+        return ans;
+    }
+    int peek() { return s1.top(); }
+    bool empty() { return s1.empty(); }
+};
+
+// Time Complexity  : O(N) for push, O(1) for pop/peek/empty
+// Space Complexity : O(N)
+// by ar-sayeem [June 07, 2026]

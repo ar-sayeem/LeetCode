42d23e1f40ff2a695f9be0d086e868c05bcf0d5b
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-06-18 10:43:10 +0600

    Time: 3 ms (19.52%), Space: 19.9 MB (89.23%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        
+        queue = deque()
+        queue.append(root)
+        ans = []
+
+        while queue:
+            level = []
+            n = len(queue)
+            for i in range(n):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            ans.append(level)
+
+        return ans
+
+# Time Complexity  : O(n)
+# Space Complexity : O(n)
+# by ar-sayeem [June 18, 2026]

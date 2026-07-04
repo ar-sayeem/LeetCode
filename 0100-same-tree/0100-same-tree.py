6f51aabdf8cf07c4d760bb6e9d4118e7df060092
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-07-04 22:51:20 +0600

    Time: 0 ms (100%), Space: 19.2 MB (71.52%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -1,31 +1,17 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:  # both tree are NULL
+        if not p and not q:
             return True
-        if not p or not q:  # one of the tree is not NULL
+
+        if not p or not q:
             return False
-        if p.val != q.val:  # value not same
+
+        if p.val != q.val:
             return False
 
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        # check left and right subtrees
+        return (self.isSameTree(p.left, q.left) and
+                self.isSameTree(p.right, q.right))
 
-
-# Time Complexity: O(n)
-# Space Complexity: O(n)
-# by ar-sayeem [February 4, 2026]
-
-
-# Time Complexity: O(n) -> O(n + m)
-# n = number of nodes in tree p
-# m = number of nodes in tree q
-
-# Space Complexity: O(n) -> O(max(h_p, h_q))
-# h_p = height of tree p
-# h_q = height of tree q
+# Time Complexity: O(N)
+# Space Complexity: O(H)
+# by ar-sayeem [July 04, 2026]

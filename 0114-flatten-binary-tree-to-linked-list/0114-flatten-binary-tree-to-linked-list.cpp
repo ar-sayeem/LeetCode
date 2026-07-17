3410f6a9bae354d73698016a3bbdc6e1e92d48b9
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-07-17 18:02:51 +0600

    Time: 0 ms (100%), Space: 17.7 MB (24.21%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    TreeNode* nextRight = NULL;
-
     void flatten(TreeNode* root) {
-        if (root == NULL) {
-            return;
+        TreeNode* curr = root;
+
+        while (curr) {
+            if (curr->left) {
+                TreeNode* prev = curr->left;
+                while (prev->right) {
+                    prev = prev->right;
+                }
+                prev->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+            curr = curr->right;
         }
-
-        flatten(root->right);
-        flatten(root->left);
-
-        root->left = NULL;
-        root->right = nextRight;
-
-        nextRight = root;
     }
 };
 
 // Time Complexity: O(N)
-// Space Complexity: O(H)
+// Space Complexity: O(1)
 // by ar-sayeem [July 17, 2026]

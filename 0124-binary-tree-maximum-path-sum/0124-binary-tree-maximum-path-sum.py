81591e8cb9530ca0ec8c429104d4259983880e70
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-07-19 12:05:35 +0600

    Time: 4 ms (94.36%), Space: 23.8 MB (91.21%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = [root.val]
+
+        def dfs(root):
+            if not root:
+                return 0
+
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
+
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+
+            return root.val + max(leftMax, rightMax)
+
+        dfs(root)
+        return res[0]
+
+# Time Complexity   : O(N)
+# Space Complexity  : O(H)
+# by ar-sayeem [July 19, 2026]

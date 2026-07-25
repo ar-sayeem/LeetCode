2309bbbe831cd7af2fe09a0526810c9a75d01020
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-07-25 09:30:20 +0600

    Time: 2 ms (25.73%), Space: 19.2 MB (88.69%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProduct(self, n: int) -> int:
+        largest = 0
+        second = 0
+
+        while n > 0:
+            digit = n % 10
+
+            if digit >= largest:
+                second = largest
+                largest = digit
+            elif digit > second:
+                second = digit
+            n //= 10
+
+        return largest * second
+        
+# Time Complexity   : O(N)
+# Space Complexity  : O(1)
+# by ar-sayeem [July 25, 2026]

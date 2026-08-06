595e3af9c763c1f316f0a49a71c90cdce5a2ad21
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-08-06 09:20:59 +0600

    Time: 0 ms (100%), Space: 19.3 MB (32.93%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -4,15 +4,15 @@ class Solution:
             product = 1
             while num > 0:
                 product *= num % 10
-                num //= 10
                 if product == 0:
                     break
+                num //= 10
             return product % t == 0
 
         while not check(n):
             n += 1
         return n
     
-# Time Complexity   : O(N)
+# Time Complexity   : O(1)
 # Space Complexity  : O(1)
 # by ar-sayeem [August 06, 2026]

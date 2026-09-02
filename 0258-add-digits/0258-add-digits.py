c6d65754dd1fe975f2076e5edf1e34ab78fe6a80
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2026-09-02 12:54:50 +0600

    Time: 0 ms (100%), Space: 19.1 MB (86.53%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -6,6 +6,6 @@ class Solution:
         # for all other numbers
         return 1 + (num - 1) % 9
 
-# Time Complexity   : O()
-# Space Complexity  : O()
+# Time Complexity   : O(1)
+# Space Complexity  : O(1)
 # by ar-sayeem [September 02, 2026]
